9449d2674d49d42ecb5ad5f937c9aa699ea5fd74
naveranoc <nicolas.a.verano@gmail.com>
2024-02-27 21:09:23 -0500

    set load packages inside setup

---
+++ setup/load_packages.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(bigrquery)
+library(dbplyr)
+
+# librerias para graficar
+library(plotly)
+library(gridExtra)
+
+#librarias para llamar a un excel
+library(googlesheets4)
